69104c8deda04c183e281d9214cb50ffdef49e80
Alexandre Hartmann <alexandre.hartmann@o2obots.com>
2023-09-20 19:49:48 -0300

    refactor: clean code

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,10 +3,16 @@ package main
 import (
 	"fmt"
 
-	config "github.com/ArthurRF/runners-api/configs"
+	"github.com/ArthurRF/runners-api/internal/entity"
 )
 
 func main() {
-	cfg, _ := config.LoadConfig(".")
-	fmt.Println("main", cfg.DBDriver)
+	user, err := entity.NewUser("Alexandre", "emailteste", "abcdefg")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("main", user)
+
+	isValid := user.ValidatePassword("abcdefg")
+	fmt.Println("main", isValid)
 }
